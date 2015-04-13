2483d6afbca28876ca3ea770cf6b2622282f74a0
tlpayne <torreyl.payne@gmail.com>
2015-04-13 02:14:23 +0000

    work on Player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,13 @@
 class Player < ActiveRecord::Base
     validates :username,    presence: true, length: { maximum: 50 }
     validates :email,       presence: true, length: { maximum: 255 }
+    def initialize(username, email)
+        @username = username
+        @email = email
+        @balance = 0
+    end
+    
+    def balance
+        @balance
+    end
 end
